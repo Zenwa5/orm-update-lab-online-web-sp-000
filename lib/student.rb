fbc4667c5b2eae65ac608ad5a0d57864089578e2
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-03-23 18:50:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,9 +56,9 @@ class Student
  end
 
  def self.new_from_db(row)
-  new_student = self.new  
-  new_song.id = row[0]
-  new_song.name =  row[1]
+  new_student = self.new
+  new_student.id = row[0]
+  new_student.name =  row[1]
   new_song.length = row[2]
   new_song
  end
